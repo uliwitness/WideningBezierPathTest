b05044e3210088f4df996745cd119e9b2212bef4
Uli Kusterer <ulivc@zathras.de>
2014-03-08 21:32:18 +0100

    Change code to flatten path. Oddly, still looks the same.

--- WideningBezierPath/ULIWideningBezierPathView.m
+++ WideningBezierPath/ULIWideningBezierPathView.m
@@ -45,70 +45,77 @@
 
 -(void)	drawRect: (NSRect)dirtyRect
 {
-	// --- Calculate start & endpoints for the lines that serve as end caps for our line shape:
-	// Luckily, the slope at the start/end of a bezer curve is the slope of the line through that endpoint and its control point:
-	// Calculate the distances of the end points:
-	NSPoint	startDist = { start.x -cp1.x, start.y -cp1.y },
-			endDist = { end.x -cp2.x, end.y -cp2.y };
-	// Now calc the same for perpendicular lines to these (the end caps of our line's shape):
-	NSPoint	orthogonalStartDist = { -startDist.y, startDist.x },
-			orthogonalEndDist = { -endDist.y, endDist.x };
-	// We know our lines go through start/end, and we know they should be startWidth/endWidth long:
-	//	Now we need to calculate endpoints for such a line, using Pythagoras (a line
-	//	could be seen as the hypothenuse of a right triangle, with startDist/endDist describing
-	//	the triangle's other two sides):
-	// First, find out how far apart our start/end point and corresponding control point really are:
-	CGFloat	orthogonalStartDistLen = sqrt(orthogonalStartDist.x*orthogonalStartDist.x +orthogonalStartDist.y*orthogonalStartDist.y);
-	CGFloat	orthogonalEndDistLen = sqrt(orthogonalEndDist.x*orthogonalEndDist.x +orthogonalEndDist.y*orthogonalEndDist.y);
-	// Now, determine what we need to divide it by to get the desired length (startWidth /2), so we can calc top & bottom relative to middle:
-	CGFloat	startDistScaleFactor = orthogonalStartDistLen / (startWidth /2);
-	CGFloat	endDistScaleFactor = orthogonalEndDistLen / (endWidth /2);
-	NSPoint	halfStartEndcapDist = { orthogonalStartDist.x / startDistScaleFactor, orthogonalStartDist.y / startDistScaleFactor };
-	NSPoint	halfEndEndcapDist = { orthogonalEndDist.x / endDistScaleFactor, orthogonalEndDist.y / endDistScaleFactor };
-
-	// Now that we know how far one half of the line is away from the center in offsets on the X/Y axes, we can calculate the start & end points of the start's end cap:
-	NSPoint	startEndcapStart = start,
-			startEndcapEnd = start;
-	startEndcapStart.x -= halfStartEndcapDist.x;
-	startEndcapStart.y -= halfStartEndcapDist.y;
-	startEndcapEnd.x += halfStartEndcapDist.x;
-	startEndcapEnd.y += halfStartEndcapDist.y;
-	
-	// And the end's end cap:
-	NSPoint	endEndcapStart = end,
-			endEndcapEnd = end;
-	endEndcapStart.x -= halfEndEndcapDist.x;
-	endEndcapStart.y -= halfEndEndcapDist.y;
-	endEndcapEnd.x += halfEndEndcapDist.x;
-	endEndcapEnd.y += halfEndEndcapDist.y;
-		
-	// --- Draw variable-width path as a shape:
-	NSBezierPath	*	segmentPath = [NSBezierPath bezierPath];
-	[segmentPath moveToPoint: startEndcapStart];
-	[segmentPath lineToPoint: startEndcapEnd];
-	[segmentPath curveToPoint: endEndcapEnd controlPoint1: cp1 controlPoint2: cp2];
-	[segmentPath lineToPoint: endEndcapStart];
-	[segmentPath curveToPoint: startEndcapStart controlPoint1: cp2 controlPoint2: cp1];
-	
-	[NSColor.purpleColor set];
-	[segmentPath fill];
-	
-	// --- Draw Quartz path this should correspond to for comparison:
 	NSBezierPath	*	originalPath = [NSBezierPath bezierPath];
 	[originalPath moveToPoint: start];
 	[originalPath curveToPoint: end
        controlPoint1: cp1
        controlPoint2: cp2];
+	NSBezierPath	*	flatPath = [originalPath bezierPathByFlatteningPath];
+	NSPoint				lastEndcapStart = NSZeroPoint,
+						lastEndcapEnd = NSZeroPoint;
+	CGFloat				segmentStartWidth = startWidth;
+	CGFloat				segmentEndWidth = startWidth;
+	NSPoint				startPoint = NSZeroPoint;
+	NSInteger			numElems = flatPath.elementCount;
+	for( NSInteger x = 0; x < numElems; x++ )
+	{
+		NSPoint				controlPoints[3] = {0};
+		NSBezierPathElement elem = [flatPath elementAtIndex: x associatedPoints: controlPoints];
+		if( elem == NSMoveToBezierPathElement )
+		{
+			startPoint = controlPoints[0];
+			segmentStartWidth = startWidth +((endWidth -startWidth) *((1.0 / numElems) *x));
+		}
+		else if( elem == NSLineToBezierPathElement )
+		{
+			segmentEndWidth = startWidth +((endWidth -startWidth) *((1.0 / numElems) * x));
+			NSPoint	endPoint = controlPoints[0];
+			NSPoint	startDist = { endPoint.x -startPoint.x, startPoint.y -endPoint.y };
+			NSPoint	orthogonalStartDist = { -startDist.y, startDist.x };
+			CGFloat	orthogonalStartDistLen = sqrt(orthogonalStartDist.x*orthogonalStartDist.x +orthogonalStartDist.y*orthogonalStartDist.y);
+			CGFloat	startDistScaleFactor = orthogonalStartDistLen / (segmentStartWidth /2);
+			NSPoint	halfStartEndcapDist = { orthogonalStartDist.x / startDistScaleFactor, orthogonalStartDist.y / startDistScaleFactor };
+
+			// Now that we know how far one half of the line is away from the center in offsets on the X/Y axes, we can calculate the start & end points of the start's end cap:
+			if( x == 1 )
+			{
+				lastEndcapStart = startPoint,
+				lastEndcapEnd = startPoint;
+				lastEndcapStart.x -= halfStartEndcapDist.x;
+				lastEndcapStart.y -= halfStartEndcapDist.y;
+				lastEndcapEnd.x += halfStartEndcapDist.x;
+				lastEndcapEnd.y += halfStartEndcapDist.y;
+			}
+			
+			// And the end's end cap:
+			NSPoint	endEndcapStart = endPoint,
+					endEndcapEnd = endPoint;
+			endEndcapStart.x -= halfStartEndcapDist.x;
+			endEndcapStart.y -= halfStartEndcapDist.y;
+			endEndcapEnd.x += halfStartEndcapDist.x;
+			endEndcapEnd.y += halfStartEndcapDist.y;
+			
+			NSBezierPath	*	segmentPath = [NSBezierPath bezierPath];
+			[segmentPath moveToPoint: lastEndcapStart];
+			[segmentPath lineToPoint: lastEndcapEnd];
+			[segmentPath lineToPoint: endEndcapEnd];
+			[segmentPath lineToPoint: endEndcapStart];
+			[segmentPath lineToPoint: lastEndcapStart];
+			
+			[NSColor.purpleColor set];
+			[segmentPath fill];
+			
+			lastEndcapStart = endEndcapStart;
+			lastEndcapEnd = endEndcapEnd;
+			startPoint = endPoint;
+			segmentStartWidth = segmentEndWidth;
+		}
+	}
 	
+	// --- Draw the Quartz path that this should correspond to, for comparison:
 	[NSColor.blackColor set];
 	[originalPath setLineWidth: 1];
 	[originalPath stroke];
-
-	[NSColor.redColor set];
-	[NSBezierPath strokeLineFromPoint: startEndcapStart toPoint: startEndcapEnd];
-
-	[NSColor.redColor set];
-	[NSBezierPath strokeLineFromPoint: endEndcapStart toPoint: endEndcapEnd];
 	
 	// --- Draw control points:
 	[NSColor.greenColor set];
